dc8fad796bd85b364e565a638b619f6cef6f04f7
EthanSeok <champ9162@naver.com>
2023-10-30 10:35:40 +0900

    Blog model 수정

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    posts = Post.objects.all()
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/index.html'
+    ordering = '-pk'
 
+class PostDetail(DetailView):
+    model = Post
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-updated_at')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#
+#         }
+#     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
     return render(
         request,
-        'blog/index.html',
+        'blog/post_detail.html',
         {
-            'posts': posts,
-
+            'post': post,
         }
     )
